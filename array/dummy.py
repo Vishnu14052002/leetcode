e916e323bb17001b659082a796dba68a02201cdd
Vishnu14052002 <vishnug14052002@gmail.com>
2026-02-10 15:31:45 +0000

    n

--- array/dummy.py
+++ array/dummy.py
@@ -5,7 +5,7 @@ class Solution:
         p2 = 0
         for i in range(len(nums1)):
             if dnum1[p1] < nums2[p2]:
-                print(dnum1[p1], nums2[p2], i, p1, "in " , i, "iteration")
+                print(dnum1[p1], nums2[p2], i, p1, "in " , i, "itation")
                 nums1[i] = dnum1[p1]
                 print(nums1, "in first")
                 p1 += 1
