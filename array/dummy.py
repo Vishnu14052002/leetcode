935ab38cd9b90a5f213f67ab26bff6d2d51c918e
Vishnu14052002 <vishnug14052002@gmail.com>
2026-02-12 15:04:36 +0000

    commits

--- array/dummy.py
+++ array/dummy.py
@@ -1,31 +1,23 @@
 class Solution:
-    def dummy(self, nums1, nums2, m, n):
-        dnum1 = nums1.copy()
+    def dummy(self, nums):
+        print(len(nums))
         p1 = 0
-        p2 = 0
-
-        for i in range(len(nums1)):
-            if p2 >= n or (p1 < m and dnum1[p1] < nums2[p2]):
-                nums1[i] = dnum1[p1]
+        for p2 in range( len(nums)):
+            if nums[p1] < nums[p2]:
                 p1 += 1
+                nums[p1] = nums[p2]
+                print(nums)
+            
 
-            else:
-                nums1[i] = nums2[p2]
-                p2 += 1
-
-            print(nums1)
+        print(nums, p1, p2)
 
 
-        print(nums1)
 
 
 
 
 obj = Solution()
-nums1 = [1,2,3,0,0,0]
-m = 3
-nums2 = [1,1,1]
-n = 3
+nums = [0,0,1,1,1,2,2,3,3,4]
 
-obj.dummy(nums1, nums2, m, n)
+obj.dummy(nums)
 
