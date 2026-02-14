aed15e63e1805e22f296e997030c1f09e72de038
Vishnu14052002 <vishnug14052002@gmail.com>
2026-02-14 12:03:04 +0000

    contionous cocunt

--- array/dummy.py
+++ array/dummy.py
@@ -1,5 +1,16 @@
 class Solution:
     def dummy(self, nums):
+        curr_count = 0
+        fin_count = 0
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                curr_count += 1
+            
+            if nums[i] != 1:
+                fin_count = curr_count
+                curr_count = 0
+        print(curr_count, fin_count)
+
 
 
 
@@ -9,7 +20,7 @@ class Solution:
 
 
 obj = Solution()
-nums = [0,0,1,1,1,2,2,3,3,4]
+nums = [1,1,0,1,1,1]
 
 obj.dummy(nums)
 
