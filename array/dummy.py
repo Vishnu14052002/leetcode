ec75b9f83b701160ac05a4db20e9ea26961f3e20
Vishnu14052002 <vishnug14052002@gmail.com>
2026-02-15 16:40:34 +0000

    co

--- array/dummy.py
+++ array/dummy.py
@@ -1,13 +1,6 @@
 class Solution:
     def dummy(self, s):
-        leng = int(len(s) / 2)
-        p2 = len(s)-1
-        print(p2)
-        for p1 in range(leng):
-            s[p1], s[p2] = s[p2], s[p1]
-            p2 -= 1
-            
-        print(s)
+]
 
 
 
