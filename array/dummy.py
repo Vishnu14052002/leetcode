6e836a017326a218630d02a0a75148635123c8cf
Vishnu14052002 <vishnug14052002@gmail.com>
2026-02-18 23:30:59 +0000

    commits

--- array/dummy.py
+++ array/dummy.py
@@ -1,14 +1,6 @@
 class Solution:
     def dummy(self, prices):
-        max_profit = 0
-        min_price = prices[0]
-        for i in range(1, len(prices)):
-            if prices[i] < min_price:
-                min_price = prices[i]
 
-            tempmax = prices[i] - min_price
-            if max_profit < tempmax:
-                max_profit = tempmax
         print(max_profit, min_price)
 
             
