2479f0e98d22dfb45f15ebb7ba679a12e0aca303
Vishnu14052002 <vishnug14052002@gmail.com>
2026-02-21 16:51:21 +0000

    commits

--- array/dummy.py
+++ array/dummy.py
@@ -1,21 +1,29 @@
 class Solution:
-    def dummy(self, nums1, nums2):
-        p1 = 0
-        p2 = 0
-        dummy = nums1.copy()
-        for i in range(len(nums1)):
-            if dummy[p1] > nums2[p2]:
-                nums1[i] = nums2[p2]
-                p2 += 1
-                print('first ',nums1, p1, p2)
+    def dummy(self, nums):
+        count = 0
+        max_count = 0
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                count += 1
+            
+            elif nums[i] == 0:
+                if count > max_count:
+                    max_count = count
+                    count = 0
+                count = 0
+        
+            if max_count > count:
+                print('x',max_count)
+                # return max_count
             else:
-                nums1[i] = dummy[p1]
-                p1 += 1
-                print('second ',nums1, p1, p2)
+                print('xx',count)
+                # return count
+
+
         
 
 
 obj = Solution()
-nums1 = [1, 3, 4, 7, 0, 0, 0, 0]
-nums2 = [2, 5, 6, 8]
-a = obj.dummy(nums1, nums2)
+nums = [1,1,0,1,1,1]
+a = obj.dummy(nums)
+# print(a)
